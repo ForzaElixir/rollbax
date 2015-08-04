aed207ab4b5401ab756b7357fb6d8952db406780
Aleksei Magusev <lexmag@me.com>
2015-08-04 22:01:54 +0200

    Publish to the Hex

--- mix.exs
+++ mix.exs
@@ -1,25 +1,37 @@
 defmodule Rollbax.Mixfile do
   use Mix.Project
 
-  def project do
+  def project() do
     [app: :rollbax,
      version: "0.0.1",
      elixir: "~> 1.0",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps]
+     description: description(),
+     package: package(),
+     deps: deps()]
   end
 
-  def application do
+  def application() do
     [applications: [:logger, :hackney, :poison],
      mod: {Rollbax, []}]
   end
 
-  defp deps do
+  defp deps() do
     [{:hackney, "~> 1.1"},
      {:poison,  "~> 1.4.0"},
 
      {:plug,   "~> 0.13.0", only: :test},
      {:cowboy, "~> 1.0.0", only: :test}]
+  end
+
+  defp description() do
+    "Exception tracking and logging from Elixir to Rollbar"
+  end
+
+  defp package() do
+    [contributors: ["Aleksei Magusev"],
+     licenses: ["ISC"],
+     links: %{"GitHub" => "https://github.com/elixir-addicts/rollbax"}]
   end
 end
