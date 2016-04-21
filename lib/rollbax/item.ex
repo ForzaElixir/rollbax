9d2d4d5b3bbc4cae093f833313e0a4e4499fc851
Sergey Novikov <novikov@inventos.ru>
2016-04-21 12:21:23 +0700

    Returned string keys back

--- lib/rollbax/item.ex
+++ lib/rollbax/item.ex
@@ -2,44 +2,44 @@ defmodule Rollbax.Item do
   def draft(token, envt) do
     {:ok, host} = :inet.gethostname
     %{
-      access_token: token,
-      data: %{
-        server: %{
-          host: List.to_string(host)
+      "access_token" => token,
+      "data" => %{
+        "server" => %{
+          "host" => List.to_string(host)
         },
-        environment: envt,
-        language: language(),
-        platform: platform(),
-        notifier: notifier()
+        "environment" => envt,
+        "language" => language(),
+        "platform" => platform(),
+        "notifier" => notifier()
       }
     }
   end
 
   def compose(draft, {level, e, stacktrace, time, meta}) do
     {occurr_data, meta} =
-      Map.pop(meta, :rollbax_occurr_data, %{})
-    Map.update!(draft, :data, fn(data) ->
+      Map.pop(meta, "rollbax_occurr_data", %{})
+    Map.update!(draft, "data", fn(data) ->
       Map.merge(occurr_data, data)
       |> put_body(e, stacktrace)
       |> put_custom(meta)
-      |> Map.put(:level, level)
-      |> Map.put(:timestamp, time)
+      |> Map.put("level", level)
+      |> Map.put("timestamp", time)
     end)
   end
 
   # If a message with no stack trace, use "message"
   defp put_body(data, e, nil) do
-    Map.put(data, :body, %{ message: %{ body: Exception.format(:error, e, nil) } })
+    Map.put(data, "body", %{ "message" => %{ "body" => Exception.format(:error, e, nil) } })
   end
 
   # If this payload is a single exception, use "trace"
   # stacktrace example: [{Test, :report, 2, [file: 'file.exs', line: 16]}]
   defp put_body(data, e, stacktrace) do
-    Map.put(data, :body, %{ trace: trace(e, stacktrace) })
+    Map.put(data, "body", %{ "trace" => trace(e, stacktrace) })
   end
 
   defp trace(e, stacktrace) do
-    %{ frames: frames(stacktrace), exception: exception(e) }
+    %{ "frames" => frames(stacktrace), "exception" => exception(e) }
   end
 
   # Required: frames
@@ -50,11 +50,11 @@ defmodule Rollbax.Item do
       %{
         # Required: filename
         # The filename including its full path
-        filename: file[:file],
+        "filename" => file[:file],
 
         # Optional: lineno
         # The line number as an integer
-        lineno: file[:line]
+        "lineno" => file[:line]
       }
     end)
   end
@@ -66,11 +66,11 @@ defmodule Rollbax.Item do
     %{
       # Required: class
       # The exception class name.
-      class: to_string(e.__struct__),
+      "class" => to_string(e.__struct__),
 
       # Optional: message
       # The exception message, as a string
-      message: e.message
+      "message" => e.message
     }
   end
 
--- test/rollbax/item_test.exs
+++ test/rollbax/item_test.exs
@@ -9,45 +9,45 @@ defmodule Rollbax.ClientTest do
 
   test "compose with stacktrace" do
     r = Item.compose(Item.draft(@access_token, Mix.env), {
-      :error, RuntimeError.exception(@message), @stacktrace, @timestamp, %{ metakey: :metaval }
+      :error, RuntimeError.exception(@message), @stacktrace, @timestamp, %{ "metakey" => "metaval" }
     })
 
-    assert r.access_token == @access_token
-    assert r.data.environment == Mix.env
-    assert r.data.level == :error
-    assert r.data.timestamp == @timestamp
-    assert String.contains?(r.data.platform, "Erlang")
-    assert String.contains?(r.data.language, "Elixir")
+    assert r["access_token"] == @access_token
+    assert r["data"]["environment"] == Mix.env
+    assert r["data"]["level"] == :error
+    assert r["data"]["timestamp"] == @timestamp
+    assert String.contains?(r["data"]["platform"], "Erlang")
+    assert String.contains?(r["data"]["language"], "Elixir")
     {:ok, host} = :inet.gethostname
-    assert r.data.server.host == to_string(host)
+    assert r["data"]["server"]["host"] == to_string(host)
 
-    trace = r.data.body.trace
-    assert trace.exception.class == "Elixir.RuntimeError"
-    assert trace.exception.message == @message
-    frame = hd(trace.frames)
-    assert frame.filename == 'file.exs'
-    assert frame.lineno == 16
-    refute r.data.body[:trace_chain]
-    refute r.data.body[:message]
-    refute r.data.body[:crash_report]
+    trace = r["data"]["body"]["trace"]
+    assert trace["exception"]["class"] == "Elixir.RuntimeError"
+    assert trace["exception"]["message"] == @message
+    frame = hd(trace["frames"])
+    assert frame["filename"] == 'file.exs'
+    assert frame["lineno"] == 16
+    refute r["data"]["body"]["trace_chain"]
+    refute r["data"]["body"]["message"]
+    refute r["data"]["body"]["crash_report"]
   end
 
   test "compose without stacktrace" do
     r = Item.compose(Item.draft(@access_token, Mix.env), {
-      :error, RuntimeError.exception(@message), nil, @timestamp, %{ metakey: :metaval }
+      :error, RuntimeError.exception(@message), nil, @timestamp, %{ "metakey" => "metaval" }
     })
 
-    assert r.access_token == @access_token
-    assert r.data.environment == Mix.env
-    assert r.data.level == :error
-    assert r.data.timestamp == @timestamp
-    assert String.contains?(r.data.platform, "Erlang")
-    assert String.contains?(r.data.language, "Elixir")
+    assert r["access_token"] == @access_token
+    assert r["data"]["environment"] == Mix.env
+    assert r["data"]["level"] == :error
+    assert r["data"]["timestamp"] == @timestamp
+    assert String.contains?(r["data"]["platform"], "Erlang")
+    assert String.contains?(r["data"]["language"], "Elixir")
     {:ok, host} = :inet.gethostname
-    assert r.data.server.host == to_string(host)
+    assert r["data"]["server"]["host"] == to_string(host)
 
-    refute r.data.body[:trace]
-    refute r.data.body[:trace_chain]
-    assert r.data.body.message.body == "** (RuntimeError) " <> @message
+    refute r["data"]["body"]["trace"]
+    refute r["data"]["body"]["trace_chain"]
+    assert r["data"]["body"]["message"]["body"] == "** (RuntimeError) " <> @message
   end
 end
