bbd2c3a47d431cb14c2d2a5b1a165065eacdeb86
Andrea Leopardi <an.leopardi@gmail.com>
2016-06-10 12:26:09 +0200

    Add a couple of tests for Rollbax.emit/3

--- test/rollbax_test.exs
+++ test/rollbax_test.exs
@@ -13,7 +13,7 @@ defmodule RollbaxTest do
     on_exit(&RollbarAPI.stop/0)
   end
 
-  test "exception report" do
+  test "report/3: reporting an error" do
     stacktrace = [{Test, :report, 2, [file: 'file.exs', line: 16]}]
     exception = RuntimeError.exception("pass")
     :ok = Rollbax.report(:error, exception, stacktrace, %{}, %{uuid: "d4c7"})
@@ -25,6 +25,32 @@ defmodule RollbaxTest do
     assert body =~ "lineno\":16"
     assert body =~ "method\":\"Test.report/2\""
     assert body =~ "uuid\":\"d4c7"
+    refute body =~ "custom"
+  end
+
+  test "report/3: reporting an exit" do
+    stacktrace = [{Test, :report, 2, [file: 'file.exs', line: 16]}]
+    :ok = Rollbax.report(:exit, :oops, stacktrace)
+    assert_receive {:api_request, body}
+    assert body =~ "level\":\"error\""
+    assert body =~ "class\":\"exit\""
+    assert body =~ "message\":\":oops\""
+    assert body =~ "filename\":\"file.exs\""
+    assert body =~ "lineno\":16"
+    assert body =~ "method\":\"Test.report/2\""
+    refute body =~ "custom"
+  end
+
+  test "report/3: reporting a throw" do
+    stacktrace = [{Test, :report, 2, [file: 'file.exs', line: 16]}]
+    :ok = Rollbax.report(:throw, :oops, stacktrace)
+    assert_receive {:api_request, body}
+    assert body =~ "level\":\"error\""
+    assert body =~ "class\":\"throw\""
+    assert body =~ "message\":\":oops\""
+    assert body =~ "filename\":\"file.exs\""
+    assert body =~ "lineno\":16"
+    assert body =~ "method\":\"Test.report/2\""
     refute body =~ "custom"
   end
 end
