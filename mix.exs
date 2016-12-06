a1e60d1c63cb9646fad4864790d452bc9e742315
Andrea Leopardi <an.leopardi@gmail.com>
2016-12-06 11:57:01 +0100

    Bump Elixir requirement to ~> 1.1

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule Rollbax.Mixfile do
   def project() do
     [app: :rollbax,
      version: @version,
-     elixir: "~> 1.0",
+     elixir: "~> 1.1",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      description: description(),
