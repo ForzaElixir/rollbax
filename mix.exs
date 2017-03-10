694f85d02de88f5ce7385302b9e37b8d90a78c99
Milad Rastian <milad@high-mobility.com>
2017-03-10 14:42:43 +0100

    update hackney dependency
    hackney 1.3.2 is from 2 years back

--- mix.exs
+++ mix.exs
@@ -25,7 +25,7 @@ defmodule Rollbax.Mixfile do
   end
 
   defp deps() do
-    [{:hackney, "~> 1.1"},
+    [{:hackney, "~> 1.7"},
      {:poison,  "~> 1.4 or ~> 2.0 or ~> 3.0"},
 
      {:ex_doc, ">= 0.0.0", only: :docs},
