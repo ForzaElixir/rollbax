7ddd78b3c0d02f4b853f5bb45fc2981776b3a3b9
Aleksei Magusev <lexmag@me.com>
2017-04-08 11:55:59 +0200

    Update ExDoc dependency

--- mix.exs
+++ mix.exs
@@ -26,12 +26,11 @@ defmodule Rollbax.Mixfile do
 
   defp deps() do
     [{:hackney, "~> 1.1"},
-     {:poison,  "~> 1.4 or ~> 2.0 or ~> 3.0"},
+     {:poison, "~> 1.4 or ~> 2.0 or ~> 3.0"},
 
-     {:ex_doc, ">= 0.0.0", only: :docs},
-     {:earmark, ">= 0.0.0", only: :docs},
+     {:ex_doc, "~> 0.14", only: :dev},
 
-     {:plug,   "~> 1.0.0", only: :test},
+     {:plug, "~> 1.0.0", only: :test},
      {:cowboy, "~> 1.0.0", only: :test}]
   end
 
