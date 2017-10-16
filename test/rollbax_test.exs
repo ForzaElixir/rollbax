89ab2b3ca0ef57aca30970af64df7f43c6e7a467
Manuel <manuel.albarran@cabify.com>
2017-10-16 23:37:00 +0200

    Adding support for all Rollbar levels and raw messages in report function.

--- test/rollbax_test.exs
+++ test/rollbax_test.exs
@@ -37,6 +37,51 @@ defmodule RollbaxTest do
     assert body =~ ~s("message":"expected a map, got: nil")
   end
 
+  test "report/3 with an error that is a message" do
+    stacktrace = [{Test, :report, 2, [file: 'file.exs', line: 16]}]
+    error = "Something was wrong"
+    :ok = Rollbax.report(:error, error, stacktrace, %{}, %{})
+    assert_receive {:api_request, body}
+    assert body =~ ~s("level":"error")
+    assert body =~ ~s("message":"Something was wrong")
+  end
+
+  test "report/3 with an debug that is a message" do
+    stacktrace = [{Test, :report, 2, [file: 'file.exs', line: 16]}]
+    message = "Settings saved"
+    :ok = Rollbax.report(:debug, message, stacktrace, %{}, %{})
+    assert_receive {:api_request, body}
+    assert body =~ ~s("level":"debug")
+    assert body =~ ~s("message":"Settings saved")
+  end
+
+  test "report/3 with an info that is a message" do
+    stacktrace = [{Test, :report, 2, [file: 'file.exs', line: 16]}]
+    message = "Login successful"
+    :ok = Rollbax.report(:info, message, stacktrace, %{}, %{})
+    assert_receive {:api_request, body}
+    assert body =~ ~s("level":"info")
+    assert body =~ ~s("message":"Login successful")
+  end
+
+  test "report/3 with an warning that is a message" do
+    stacktrace = [{Test, :report, 2, [file: 'file.exs', line: 16]}]
+    message = "Unexpected input"
+    :ok = Rollbax.report(:warning, message, stacktrace, %{}, %{})
+    assert_receive {:api_request, body}
+    assert body =~ ~s("level":"warning")
+    assert body =~ ~s("message":"Unexpected input")
+  end
+
+  test "report/3 with an critical that is a message" do
+    stacktrace = [{Test, :report, 2, [file: 'file.exs', line: 16]}]
+    error = "Something is broken"
+    :ok = Rollbax.report(:critical, error, stacktrace, %{}, %{})
+    assert_receive {:api_request, body}
+    assert body =~ ~s("level":"critical")
+    assert body =~ ~s("message":"Something is broken")
+  end
+
   test "report/3 with an exit" do
     stacktrace = [{Test, :report, 2, [file: 'file.exs', line: 16]}]
     :ok = Rollbax.report(:exit, :oops, stacktrace)
